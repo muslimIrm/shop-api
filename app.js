37da3fc9b50cb129afbbc9b8378285fd0fd0747b
Muslim <max2dgame12@gmail.com>
2025-11-28 11:44:45 +0300

    fix some Bugs

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const path = require("path")
 const books = require("./routes/books")
 const cartUsers = require("./routes/cartUsers")
 const connectionMongoDB = require("./mongooseDB/ConnectionMongoose");
+const cors = require("cors")
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 app.use("/upload/images/",express.static(path.join(__dirname, "tmp")))
 
 app.use(books)
